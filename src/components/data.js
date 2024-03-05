6935eef06f8eb0e4af46d1d4346eca8655019505
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-03-05 11:39:18 +0600

    commit master

---
+++ src/components/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id : '1',
+        question: "what are accordion components?",
+        answer: "accordion components are user interface elements used for orgar",
+    },
+    {
+        id : '2',
+        question: "what are they used for?",
+        answer: "They are commonly employed in various context, including FAQs",
+    },
+    {
+        id : '3',
+        question: "Accordion as a musical instrument",
+        answer: "The accordion is a musical instrument with a keybord and bellow"
+    },
+    {
+        id : '4',
+        question: "Can I create an accordion compnent with a different free",
+        answer: "Yes of course, it is vary possible to create and accordion an accordion components",
+    },
+];
+export default data;
---
+++ src/components/index.jsx
@@ -0,0 +1,60 @@
+//dingle selaction
+//multiple selection
+
+import { useState } from "react";
+import data from "./data";
+import './styles.css'
+
+export default function Accordian() {
+
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+    function handleSinglesSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    function handleMultiSelection(getCurrentId) {
+        let cpyMutiple = [...multiple];
+        const findIddexOfCurrentId = cpyMutiple.indexOf(getCurrentId)
+        console.log(findIddexOfCurrentId);
+        if (findIddexOfCurrentId === -1) cpyMutiple.push(getCurrentId)
+        else cpyMutiple.splice(findIddexOfCurrentId, 1)
+
+        setMultiple(cpyMutiple);
+    }
+
+    console.log(selected);
+
+    return <div className="wrapper">
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
+        <div className="accordian">
+            {
+                data && data.length > 0 ? (
+                    data.map((dataItem) => (
+                        <div className="item">
+                            <div onClick={enableMultiSelection
+                                ? () => handleMultiSelection(dataItem.id)
+                                : () => handleSinglesSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                                {
+                                    enableMultiSelection ?
+                                        multiple.indexOf(dataItem.id) !== -1 &&
+                                        <div className="content">{dataItem.answer}</div> :
+                                        selected === dataItem.id && <div className="content">{dataItem.answer}</div>
+                                }
+                                {/* {
+                                    selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? (
+                                        <div className="content">{dataItem.answer}</div>
+                                     ) : null
+                                } */}
+                            </div>
+                        </div>
+                    ))
+                ) : (<div>No data found !</div>
+                )}
+        </div>
+    </div>
+};
