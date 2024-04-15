320cb64a90104c2a13e6b20858fb6edb98eba6c8
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-04-15 16:23:28 +0600

    commit -m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
-import Accordian from './components';
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider'
 
 function App() {
   return (
     <div className="App">
       {/* Accordian components */}
-      <Accordian />
+      {/* <Accordian /> */}
+
+      {/* Random color component*/}
+      {/* <RandomColor /> */}
+
+      {/* Star rating component */}
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
